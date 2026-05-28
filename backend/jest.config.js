eb3ac03a0e7ca31135c88938a10b83b8021bac91
Rodrigo Naray <rodrigo.naray@fing.edu.uy>
2026-05-28 18:26:43 -0300

    chore: configurar Jest para tests unitarios y e2e

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: 'test/.*\\.(spec|e2e-spec)\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.(t|j)s',
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '^@modules/(.*)$': '<rootDir>/src/modules/$1',
+  },
+};
